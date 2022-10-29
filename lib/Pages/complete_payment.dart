9c13130cdc26129af43cbecc31c6b0f8aa7d2f79
bambangw665 <bambangw665@gmail.com>
2022-10-30 00:59:54 +0700

    Add Animate Complete Payment

--- lib/Pages/complete_payment.dart
+++ lib/Pages/complete_payment.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rive/rive.dart';
 import '../constants.dart';
 import '../Pages/homePage.dart';
 
@@ -14,6 +17,88 @@ class CompletePaymentScreen extends StatefulWidget {
 
 class _CompletePaymentScreenState extends State<CompletePaymentScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    StarProgrss();
+  }
+
+  StarProgrss() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
+        return CompliteOrder();
+      }));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        backgroundColor: kPrimaryColor,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 0.7.sh,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 0.3.sh,
+                      width: double.infinity,
+                      child: RiveAnimation.asset(
+                        "assets/rive/sounstore_orderanimation.riv",
+                        fit: BoxFit.contain,
+                        animations: ["book"],
+                        artboard: "OrderProgress",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    Text(
+                      "Order Progress",
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Text(
+                      "Lorem Ipsum is simply dummy \n text of the printing and \n typesetting industry. ",
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class CompliteOrder extends StatefulWidget {
+  const CompliteOrder({Key? key}) : super(key: key);
+
+  @override
+  State<CompliteOrder> createState() => _CompliteOrderState();
+}
+
+class _CompliteOrderState extends State<CompliteOrder> {
+  @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
@@ -23,16 +108,24 @@ class _CompletePaymentScreenState extends State<CompletePaymentScreen> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Container(
-                height: 0.5.sh,
-                // color: Colors.grey,
+                height: 0.6.sh,
                 width: double.infinity,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    SvgPicture.asset("assets/svg/icon_completeProcess.svg"),
+                    Container(
+                      height: 0.3.sh,
+                      width: double.infinity,
+                      child: RiveAnimation.asset(
+                        "assets/rive/sounstore_orderanimation.riv",
+                        fit: BoxFit.contain,
+                        animations: ["compliteOrder"],
+                        artboard: "OurderComplite",
+                      ),
+                    ),
                     SizedBox(
-                      height: 20.h,
+                      height: 10.h,
                     ),
                     Text(
                       "Order Complete",
@@ -74,7 +167,7 @@ class _CompletePaymentScreenState extends State<CompletePaymentScreen> {
                     style: TextStyle(
                         fontSize: 20,
                         color: kPrimaryColor,
-                        fontWeight: FontWeight.w300),
+                        fontWeight: FontWeight.w500),
                   ),
                 ),
               ),
